10be6e4f82366322ebb224ec77efdd0db58ca68c
rioogp <pucuk4us@gmail.com>
2024-03-15 00:55:58 +0700

    feat: add Testimonial

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
     },
-  });
-
-  $(".slick-prev").click(function () {
-    $(".owl-carousel").trigger("prev.owl.carousel");
-  });
-
-  $(".slick-next").click(function () {
-    $(".owl-carousel").trigger("next.owl.carousel");
-  });
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: true,
+    },
+  },
 });
